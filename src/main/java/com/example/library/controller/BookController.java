88e9504ea3c489451f94d80d5275562ee91f85a6
adriellypasetto <adriellypasetto@gmail.com>
2023-11-18 16:18:09 -0300

    implementação listar livros

--- src/main/java/com/example/library/controller/BookController.java
+++ src/main/java/com/example/library/controller/BookController.java
@@ -3,12 +3,14 @@ package com.example.library.controller;
 import com.example.library.domain.Book;
 import com.example.library.dto.CreateBookDto;
 import com.example.library.service.bookService.CreateBookService;
+import com.example.library.service.bookService.ListBookService;
 import com.example.library.service.bookService.SearchBookService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -17,6 +19,7 @@ import reactor.core.publisher.Mono;
 public class BookController {
     private CreateBookService createBookService;
     private SearchBookService searchBookService;
+    private ListBookService listBookService;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -27,5 +30,10 @@ public class BookController {
     @GetMapping("/{id}")
     public Mono<Book> search(@PathVariable String id){
         return searchBookService.searchBookId(id);
+    }
+
+    @GetMapping("/list")
+    public Flux<Book> list(){
+        return listBookService.listBook();
     }
 }
